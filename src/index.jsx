44e90199426033f0f9f3a63609a5f993d119d3a2
muh5329 <106366024+muh5329@users.noreply.github.com>
2023-04-25 09:35:15 -0400

    remove unused import
    remove semantic css from imports

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,4 @@
 import './index.css'
-import 'semantic-ui-css/semantic.min.css';
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
